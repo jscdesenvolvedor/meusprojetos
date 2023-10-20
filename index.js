427f95b7369d08bd0312fcea1c12f3a0b2976b4b
Jadilson <j_adi_cardoso@hotmail.com>
2023-10-20 18:50:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+IMC sigla para índice de massa Corpórea
+Fórnula : IMC = Peso / altura x altura
+Abaixo de  17    |  Muito abaixo do peso
+Entre 17 e 18.49  | Abaixo do peso
+Entre 18,5 e 24.99 | Peso Normal
+Entre 25 e 29.99   | Acima do Peso
+Entre 30 e 34.99  | Obesidade 1
+Entre 35 e 39.99   | Obesidade 2
+*/
+
+const peso = 80;
+const altura= 1.87;
+const imc = Number ((peso / (altura * altura )).toFixed(2));
+
+console.log(typeof imc);
+switch (true) {
+    case imc < 17:
+
+        console.log('Muito abaixo do peso'); 
+        break;
+
+    case imc >= 17 && imc <= 18.49 :
+
+        console.log('Abaixo do peso'); 
+    break;
+
+    case imc >= 18.5 && imc <= 24.99 :
+
+        console.log('Peso Normal'); 
+    break;
+
+    case imc >= 25 && imc <= 29.99 :
+
+        console.log('Acima do Peso'); 
+    break;
+
+    case imc >= 30 && imc <= 34.99 :
+
+        console.log('Obesidade l'); 
+    break;
+
+    case imc >= 35 && imc <= 39.99 :
+
+        console.log('Obesidade ll'); 
+    break;
+
+    default:(false)
+        console.log('Não se encaixa nas opçoes!!');
+        break;
+}
+
